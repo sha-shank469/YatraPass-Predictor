934e306b1a785d62274a5b0847bc204ac9c85018
Shashank.Mishra <Shashank.Mishra@isourse.com>
2025-05-19 02:41:23 +0530

    .

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "us_visa",
+    version = "0.0.0",
+    author = "shashank",
+    author_email = "shashankmishra892@gmail.com",
+    packages = find_packages()
+)
