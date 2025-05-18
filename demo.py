5fc7fa8012ae4eb099d57b728d5cf80ad6276c1b
Shashank.Mishra <Shashank.Mishra@isourse.com>
2025-05-19 03:08:28 +0530

    .

--- demo.py
+++ demo.py
@@ -1,4 +1,13 @@
 from us_visa.logger import logging
 
+from us_visa.exception import USvisaException
+import sys
 
-logging.info("Welcome to our custom log")
+
+# logging.info("Welcome to our custom log")
+
+try:
+    a = 2/0
+
+except Exception as e:
+    raise USvisaException(e,sys)
