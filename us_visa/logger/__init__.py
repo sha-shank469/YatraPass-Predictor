310a31154f584401e16edd796c96030ee96ba4b1
Shashank.Mishra <Shashank.Mishra@isourse.com>
2025-05-19 03:01:21 +0530

    .

---
+++ us_visa/logger/__init__.py
@@ -0,0 +1,20 @@
+import logging
+import os
+
+from from_root import from_root
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+log_dirs = 'logs'
+
+logs_path = os.path.join(from_root(), log_dirs, LOG_FILE)
+
+os.makedirs(log_dirs, exist_ok= True)
+
+
+logging.basicConfig(
+    filename=logs_path,
+    format="[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
+    level=logging.DEBUG,
+)
